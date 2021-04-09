ce76f85683fcb216f59988d12995d4cca960ef1f
jinju <gg66477@gmail.com>
2021-04-09 15:00:10 +0900

    add:react

---
+++ src/main/java/com/jinju/demo/conifg/config/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jinju.demo.conifg.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
